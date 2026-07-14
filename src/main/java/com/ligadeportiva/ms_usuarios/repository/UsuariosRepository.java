3c4dbfa1109c7e5e0921d0c15615b6a9bce2ac63
Naoshinaps <n.paillallao.17@gmail.com>
2026-07-14 12:48:24 -0400

    respositorio listo

--- src/main/java/com/ligadeportiva/ms_usuarios/repository/UsuariosRepository.java
+++ src/main/java/com/ligadeportiva/ms_usuarios/repository/UsuariosRepository.java
@@ -1,4 +1,18 @@
 package com.ligadeportiva.ms_usuarios.repository;
 
-public interface UsuariosRepository {
+import com.ligadeportiva.ms_usuarios.modelo.Usuarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+
+    Optional<Usuarios> findByCorreoUsuarios(String correo);
+
+    Optional<Usuarios> findByRutUsuarios(String rut);
+
+    boolean existsByCorreoUsuarios(String correo);
+
+    boolean existsByRutUsuarios(String rut);
+
 }
