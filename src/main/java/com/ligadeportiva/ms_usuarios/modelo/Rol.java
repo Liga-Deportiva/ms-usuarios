512095f44bc7ad0ddc3640edab4e1fb76171c5b7
Naoshinaps <n.paillallao.17@gmail.com>
2026-07-14 12:39:23 -0400

    clase usuario lista

---
+++ src/main/java/com/ligadeportiva/ms_usuarios/modelo/Rol.java
@@ -0,0 +1,13 @@
+package com.ligadeportiva.ms_usuarios.modelo;
+
+public enum Rol {
+    JUGADOR,
+    ENTRENADOR,
+    ARBITRO,
+    DIRIGENTE,
+    DELEGADO,
+    DIRECTOR,
+    TESORERO,
+    SECRETARIO,
+    SIN_ROL
+}
--- src/main/java/com/ligadeportiva/ms_usuarios/modelo/Usuarios.java
+++ src/main/java/com/ligadeportiva/ms_usuarios/modelo/Usuarios.java
@@ -1,4 +1,31 @@
 package com.ligadeportiva.ms_usuarios.modelo;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "usuarios")
 public class Usuarios {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long idUsuarios;
+    private String nombreUsuarios;
+    private String apellidoUsuarios;
+
+    @Column (unique = true)
+    private String rutUsuarios;
+
+    @Column (unique = true)
+    private String correoUsuarios;
+
+    private LocalDate fechaNacimiento;
+    private String contraseñaUsuarios;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
 }
